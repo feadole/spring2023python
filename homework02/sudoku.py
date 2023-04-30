f99220d57ab9269076723e68e715b05df5f0f33a
feadole <adolefaithene@gmail.com>
2023-04-30 20:33:06 +0300

    test

--- homework02/sudoku.py
+++ homework02/sudoku.py
@@ -1,4 +1,5 @@
 import pathlib
+import random
 import typing as tp
 
 T = tp.TypeVar("T")
@@ -42,8 +43,10 @@ def group(values: tp.List[T], n: int) -> tp.List[tp.List[T]]:
     >>> group([1,2,3,4,5,6,7,8,9], 3)
     [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
     """
-    return values
-
+    answer = []
+    for i in range(n):
+        answer.append(values[i * n: (i + 1) * n])
+    return answer
 
 def get_row(grid: tp.List[tp.List[str]], pos: tp.Tuple[int, int]) -> tp.List[str]:
     """Возвращает все значения для номера строки, указанной в pos
@@ -55,7 +58,8 @@ def get_row(grid: tp.List[tp.List[str]], pos: tp.Tuple[int, int]) -> tp.List[str
     >>> get_row([['1', '2', '3'], ['4', '5', '6'], ['.', '8', '9']], (2, 0))
     ['.', '8', '9']
     """
-    pass
+    row = pos[0]
+    return grid[row]
 
 
 def get_col(grid: tp.List[tp.List[str]], pos: tp.Tuple[int, int]) -> tp.List[str]:
@@ -68,7 +72,8 @@ def get_col(grid: tp.List[tp.List[str]], pos: tp.Tuple[int, int]) -> tp.List[str
     >>> get_col([['1', '2', '3'], ['4', '5', '6'], ['.', '8', '9']], (0, 2))
     ['3', '6', '9']
     """
-    pass
+    col = pos[1]
+    return [grid[row][col] for row in range(9)]
 
 
 def get_block(grid: tp.List[tp.List[str]], pos: tp.Tuple[int, int]) -> tp.List[str]:
@@ -82,7 +87,16 @@ def get_block(grid: tp.List[tp.List[str]], pos: tp.Tuple[int, int]) -> tp.List[s
     >>> get_block(grid, (8, 8))
     ['2', '8', '.', '.', '.', '5', '.', '7', '9']
     """
-    pass
+    block_row = pos[0] // 3  # Integer division to determine the block's row
+    block_col = pos[1] // 3  # Integer division to determine the block's column
+
+    values = []
+    for i in range(3):
+        for j in range(3):
+            value = grid[block_row * 3 + i][block_col * 3 + j]
+            values.append(value)
+
+    return values
 
 
 def find_empty_positions(grid: tp.List[tp.List[str]]) -> tp.Optional[tp.Tuple[int, int]]:
@@ -90,12 +104,16 @@ def find_empty_positions(grid: tp.List[tp.List[str]]) -> tp.Optional[tp.Tuple[in
 
     >>> find_empty_positions([['1', '2', '.'], ['4', '5', '6'], ['7', '8', '9']])
     (0, 2)
-    >>> find_empty_positions([['1', '2', '3'], ['4', '.', '6'], ['7', '8', '9']])
+     >>> find_empty_positions([['1', '2', '3'], ['4', '.', '6'], ['7', '8', '9']])
     (1, 1)
     >>> find_empty_positions([['1', '2', '3'], ['4', '5', '6'], ['.', '8', '9']])
     (2, 0)
     """
-    pass
+    for i in range(9):
+        for j in range(9):
+            if grid[i][j] == '.':
+                return i, j
+    return None
 
 
 def find_possible_values(grid: tp.List[tp.List[str]], pos: tp.Tuple[int, int]) -> tp.Set[str]:
@@ -109,7 +127,15 @@ def find_possible_values(grid: tp.List[tp.List[str]], pos: tp.Tuple[int, int]) -
     >>> values == {'2', '5', '9'}
     True
     """
-    pass
+    row_values = set(grid[pos[0]])
+    col_values = set(grid[i][pos[1]] for i in range(9))
+    block_values = set(
+        grid[i][j]
+        for i in range((pos[0] // 3) * 3, (pos[0] // 3) * 3 + 3)
+        for j in range((pos[1] // 3) * 3, (pos[1] // 3) * 3 + 3)
+    )
+    all_values = set(str(i) for i in range(1, 10))
+    return all_values - row_values - col_values - block_values
 
 
 def solve(grid: tp.List[tp.List[str]]) -> tp.Optional[tp.List[tp.List[str]]]:
@@ -125,13 +151,57 @@ def solve(grid: tp.List[tp.List[str]]) -> tp.Optional[tp.List[tp.List[str]]]:
     >>> solve(grid)
     [['5', '3', '4', '6', '7', '8', '9', '1', '2'], ['6', '7', '2', '1', '9', '5', '3', '4', '8'], ['1', '9', '8', '3', '4', '2', '5', '6', '7'], ['8', '5', '9', '7', '6', '1', '4', '2', '3'], ['4', '2', '6', '8', '5', '3', '7', '9', '1'], ['7', '1', '3', '9', '2', '4', '8', '5', '6'], ['9', '6', '1', '5', '3', '7', '2', '8', '4'], ['2', '8', '7', '4', '1', '9', '6', '3', '5'], ['3', '4', '5', '2', '8', '6', '1', '7', '9']]
     """
-    pass
+    # Find an empty position
+    empty_position = find_empty_positions(grid)
+
+    # If there are no more empty positions, the puzzle is solved
+    if empty_position is None:
+        return grid
+
+    row, col = empty_position
+
+    # Find all possible values for the empty position
+    possible_values = find_possible_values(grid, empty_position)
+
+    # Try each possible value
+    for value in possible_values:
+        grid[row][col] = value
+
+        # Recursively solve the remaining puzzle
+        if solve(grid) is not None:
+            return grid
+
+        # If the current value leads to an invalid solution, backtrack
+        grid[row][col] = '.'
+
+    # If no solution is found, return None
+    return None
 
 
 def check_solution(solution: tp.List[tp.List[str]]) -> bool:
     """ Если решение solution верно, то вернуть True, в противном случае False """
     # TODO: Add doctests with bad puzzles
-    pass
+    for row in solution:
+        if len(row) != len(set(row)):
+            return False
+
+        # Check if each column contains unique elements
+    for col in range(9):
+        column = [solution[row][col] for row in range(9)]
+        if len(column) != len(set(column)):
+            return False
+
+        # Check if each 3x3 block contains unique elements
+    for block_row in range(3):
+        for block_col in range(3):
+            block = []
+            for row in range(block_row * 3, (block_row + 1) * 3):
+                for col in range(block_col * 3, (block_col + 1) * 3):
+                    block.append(solution[row][col])
+            if len(block) != len(set(block)):
+                return False
+
+    return True
 
 
 def generate_sudoku(N: int) -> tp.List[tp.List[str]]:
@@ -156,7 +226,22 @@ def generate_sudoku(N: int) -> tp.List[tp.List[str]]:
     >>> check_solution(solution)
     True
     """
-    pass
+    if not (0 <= N <= 81):
+        raise ValueError("Invalid number of elements")
+
+    grid = [['.' for _ in range(9)] for _ in range(9)]
+    solve(grid)  # Fill the grid with a valid Sudoku solution
+    positions = [(row, col) for row in range(9) for col in range(9)]
+    random.shuffle(positions)
+
+    for row, col in positions:
+        if N == 0:
+            break
+        value = grid[row][col]
+        grid[row][col] = '.'
+        N -= 1
+
+    return grid
 
 
 if __name__ == "__main__":
